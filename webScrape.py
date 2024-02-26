4ad99440805f4d891683c3d94531152b96bfdc91
ICLimon <149421819+ICLimon@users.noreply.github.com>
2024-02-26 14:20:28 +0000

    Added Beautiful Soup

--- webScrape.py
+++ webScrape.py
@@ -1,2 +1,17 @@
 import requests
+import sys
 from bs4 import BeautifulSoup
+
+URL = str(sys.argv[1])
+webPage = requests.get(URL)
+
+
+soup = BeautifulSoup(webPage.content, 'html.parser')
+
+print(soup)
+
+
+
+
+
+
