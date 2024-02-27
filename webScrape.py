da560f26eee9aaccc9baaa4c9d1cdfd26ad802f1
ICLimon <149421819+ICLimon@users.noreply.github.com>
2024-02-26 22:53:41 -0600

    Update webScrape.py

--- webScrape.py
+++ webScrape.py
@@ -10,7 +10,24 @@ soup = BeautifulSoup(webPage.content, 'html.parser')
 
 print(soup)
 
+def product(soup)
+  data_str = ""
+  productInfo = []
+   for item in soup.find_all("ul", class_="a-unorderedlist a-nostyle\a-vertical a-spacing-none detail-bullet-list"):
+     data_str = data_str + item.get_text() 
+     productInfo.append(data_str.split("\n")) 
+      data_str = ""
+  return productInfo
 
+productResult = product(soup)
+for item in pro_result: 
+    for j in item: 
+        if j is "": 
+            pass
+        else: 
+            print(j)
+
+  
 
 
 
