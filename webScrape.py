f4f52156404a0c7d191fc13be3c0b82374e8e2c4
ICLimon <149421819+ICLimon@users.noreply.github.com>
2024-02-27 08:49:32 -0600

    Update webScrape.py

--- webScrape.py
+++ webScrape.py
@@ -2,7 +2,11 @@ import requests
 import sys
 from bs4 import BeautifulSoup
 
-URL = str(sys.argv[1])
+URL = "https://www.amazon.in/Columbia-Mens-wind-\ 
+resistant-Glove/dp/B0772WVHPS/?_encoding=UTF8&pd_rd\ 
+_w=d9RS9&pf_rd_p=3d2ae0df-d986-4d1d-8c95-aa25d2ade606&pf\ 
+_rd_r=7MP3ZDYBBV88PYJ7KEMJ&pd_rd_r=550bec4d-5268-41d5-\ 
+87cb-8af40554a01e&pd_rd_wg=oy8v8&ref_=pd_gw_cr_cartx&th=1"
 webPage = requests.get(URL)
 
 
